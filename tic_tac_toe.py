e643ab21efabddfe5b1fc73446b255d1c1415e7f
Urgith <patrykolejniczak7@gmail.com>
2022-04-21 18:42:01 +0200

    PEP8: bloki kodu tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,11 @@
 import pygame
 
+
 def quit():
-    import sys; pygame.quit(); sys.exit(0)
+    import sys
+
+    pygame.quit()
+    sys.exit(0)
 
 
 class Game():
@@ -25,7 +29,7 @@ class Game():
 
     def creating_fields(self, size, screen):
         for i in range(size**2):
-            self.fields.append(pygame.Rect(int(i*self.full_field_size) % screen, self.full_field_size*((i*self.full_field_size) // screen), self.field_size, self.field_size))
+            self.fields.append(pygame.Rect(int(i * self.full_field_size) % screen, self.full_field_size * ((i*self.full_field_size) // screen), self.field_size, self.field_size))
 
     def drawing_fields(self):
         for field in self.fields:
@@ -34,10 +38,11 @@ class Game():
     def drawing_circles_and_crosses(self):
         for i, position in enumerate(self.positions):
             if position == 1:
-                pygame.draw.circle(self.game_screen, (0, 0, 0), (self.fields[i].x + self.field_size//2, self.fields[i].y + self.field_size//2), self.field_size // 3, int(self.full_field_size / 30) + 1)
+                pygame.draw.circle(self.game_screen, (0, 0, 0), (self.fields[i].x + (self.field_size // 2), self.fields[i].y + (self.field_size // 2)), self.field_size // 3, int(self.full_field_size / 30) + 1)
+
             elif position == 0:
-                pygame.draw.line(self.game_screen, (0, 0, 0), (self.fields[i].x + self.field_size//8, self.fields[i].y + self.field_size//8), (self.fields[i].x + self.field_size*7 // 8, self.fields[i].y + self.field_size*7 // 8), int(self.full_field_size / 30) + 1)
-                pygame.draw.line(self.game_screen, (0, 0, 0), (self.fields[i].x + self.field_size//8, self.fields[i].y + self.field_size*7 // 8), (self.fields[i].x + self.field_size*7 // 8, self.fields[i].y + self.field_size//8), int(self.full_field_size / 30) + 1)
+                pygame.draw.line(self.game_screen, (0, 0, 0), (self.fields[i].x + (self.field_size // 8), self.fields[i].y + (self.field_size // 8)), (self.fields[i].x + (self.field_size*7) // 8, self.fields[i].y + (self.field_size*7) // 8), int(self.full_field_size / 30) + 1)
+                pygame.draw.line(self.game_screen, (0, 0, 0), (self.fields[i].x + (self.field_size // 8), self.fields[i].y + (self.field_size*7) // 8), (self.fields[i].x + (self.field_size*7) // 8, self.fields[i].y + (self.field_size // 8)), int(self.full_field_size / 30) + 1)
 
     def creating_wins(self):
         self.wins = {}
@@ -45,24 +50,26 @@ class Game():
 
         for i in range(self.size):
             for j in range(self.size - self.win + 1):
-                row = list(range(self.size*i + j, self.size*i + self.win + j))
+                row = list(range((self.size*i) + j, (self.size*i) + self.win + j))
                 rows.append(row)
 
         for i in range(self.size):
             for j in range(self.size - self.win + 1):
-                row = list(range(self.size*j + i, self.size*j + self.size*self.win, self.size))
+                row = list(range((self.size*j) + i, (self.size*j) + (self.size * self.win), self.size))
                 rows.append(row)
 
         for i in range(2*(self.size - self.win) + 1):
             multiplier = self.size - self.win + 1 - abs(i - (self.size - self.win))
+
             for j in range(multiplier):
                row = list(range(self.function1(i) + j*(self.size + 1), self.function1(i) + (self.win + j - 1)*(self.size + 1) + 1, self.size + 1))
                rows.append(row)
 
         for i in range(2*(self.size - self.win) + 1):
             multiplier = self.size - self.win + 1 - abs(i - (self.size - self.win))
+
             for j in range(multiplier):
-               row = list(range(self.function2(i) - j*(self.size - 1), self.function2(i) - (self.win +j - 1)*(self.size - 1) - 1, 1 - self.size))
+               row = list(range(self.function2(i) - j*(self.size - 1), self.function2(i) - (self.win + j - 1)*(self.size - 1) - 1, 1 - self.size))
                rows.append(row)
 
         for i in range(self.size**2):
@@ -75,32 +82,38 @@ class Game():
 
     def function1(self, n):
         max = (2*(self.size - self.win) + 1) // 2
+
         if n <= max:
-            return self.size*(max - n)
+            return self.size * (max - n)
         else:
             return n - max
 
     def function2(self, n):
         max = (2*(self.size - self.win) + 1) // 2
+
         if n <= max:
-            return 2*sum(range(self.size)) - self.size*(max - n)
+            return 2*sum(range(self.size)) - (self.size * (max - n))
         else:
             return 2*sum(range(self.size)) + n - max
 
     def checking_win(self, check):
         for win in self.wins[check]:
             counter = 0
+
             for pos in win:
                 if self.positions[pos] == 0:
                     counter += 1
+
             if counter == len(win):
                 return 'Cross won'
 
         for win in self.wins[check]:
             counter = 0
+
             for pos in win:
                 if self.positions[pos] == 1:
                     counter += 1
+
             if counter == len(win):
                 return 'Circle won'
 
@@ -111,6 +124,7 @@ while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONUP and event.button==1:
             mouse_position = pygame.mouse.get_pos()
+
             for i, field in enumerate(game.fields):
                 if field.collidepoint(mouse_position) and game.positions[i] == -1:
                     game.positions[i] = game.turn % 2
@@ -126,6 +140,7 @@ while True:
                         print('Draw')
                         game = Game(800, 3, 3)
 
-        elif event.type == pygame.QUIT: quit()
+        elif event.type == pygame.QUIT:
+            quit()
 
     pygame.display.flip()
